3e5c6244d2022640e842517e6bd33f29804cd276
Filip7243 <filip7243@gmail.com>
2022-10-29 15:57:17 +0200

    added basic functionality

--- src/main/java/com/fxproject/unidashboard/utils/HibernateUtils.java
+++ src/main/java/com/fxproject/unidashboard/utils/HibernateUtils.java
@@ -1,4 +1,19 @@
 package com.fxproject.unidashboard.utils;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class HibernateUtils {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("main-persistence-unit");
+    private static EntityManager em;
+
+    static {
+        em = emf.createEntityManager();
+    }
+
+    public static EntityManager getEntityManager() {
+        return em;
+    }
 }
