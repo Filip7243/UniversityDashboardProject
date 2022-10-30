47462da58a00198ad2dcb7534854f0123a0cc579
Filip7243 <filip7243@gmail.com>
2022-10-30 12:41:30 +0100

    added first logic to StuentService and controller

--- src/main/java/com/fxproject/unidashboard/HelloController.java
+++ src/main/java/com/fxproject/unidashboard/HelloController.java
@@ -9,19 +9,25 @@ import com.fxproject.unidashboard.utils.HibernateUtils;
 import jakarta.persistence.EntityManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField idInput;
     @FXML
     private ListView<StudentDto> studentsDtoList;
+    @FXML
+    private Button findButton;
 
     private StudentService studentService;
     private List<StudentDto> students;
@@ -29,10 +35,27 @@ public class HelloController {
     public void initialize() {
         this.studentService = new StudentService(HibernateUtils.getEntityManager());
         this.students = FXCollections.observableList(studentService.getAllStudents());
+
+        // input for finding
+        findButton.setDisable(true);
+        idInput.textProperty().addListener((obs, oldText, newText) -> {
+            findButton.setDisable(obs.getValue().trim().isEmpty());
+        });
     }
 
     @FXML
     protected void onHelloButtonClick() {
         studentsDtoList.setItems((ObservableList<StudentDto>) students);
+    }
+
+    @FXML
+    protected void onFindButtonClick() {
+        Long id = Long.parseLong(idInput.getText());
+        StudentDto studentWithId = studentService.getStudentWithId(id);
+        ArrayList<StudentDto> list = new ArrayList<>();
+        ObservableList<StudentDto> s = FXCollections.observableList(list);
+        s.add(studentWithId);
+
+        studentsDtoList.setItems(s);
     }
 }
--- src/main/java/com/fxproject/unidashboard/service/StudentService.java
+++ src/main/java/com/fxproject/unidashboard/service/StudentService.java
@@ -9,6 +9,7 @@ import jakarta.persistence.EntityManager;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static com.fxproject.unidashboard.mapper.StudentMapper.mapToStudentDtos;
@@ -24,27 +25,28 @@ public class StudentService {
     }
 
     public List<StudentDto> getAllStudents() {
-        em.getTransaction().begin();
-        studentRepository.saveStudent(new Student(null, UUID.randomUUID().toString(), "Filip", "Carl", "Doe",
-                "filip@mail.com", "fk122941@stud.ur.edu.pl",
-                LocalDateTime.now(), "Cracow", "1231231231",
-                "32178352813", LocalDateTime.now(), LocalDateTime.now(), true, 1L));
-        studentRepository.saveStudent(new Student(null, UUID.randomUUID().toString(), "Zibi", "Carl", "Doe",
-                "filip@mail.com", "fk122941@stud.ur.edu.pl",
-                LocalDateTime.now(), "Cracow", "1231231231",
-                "32178352813", LocalDateTime.now(), LocalDateTime.now(), true, 1L));
-        studentRepository.saveStudent(new Student(null, UUID.randomUUID().toString(), "Mark", "Carl", "Doe",
-                "filip@mail.com", "fk122941@stud.ur.edu.pl",
-                LocalDateTime.now(), "Cracow", "1231231231",
-                "32178352813", LocalDateTime.now(), LocalDateTime.now(), true, 1L));
-        studentRepository.saveStudent(new Student(null, UUID.randomUUID().toString(), "Essa", "Carl", "Doe",
-                "filip@mail.com", "fk122941@stud.ur.edu.pl",
-                LocalDateTime.now(), "Cracow", "1231231231",
-                "32178352813", LocalDateTime.now(), LocalDateTime.now(), true, 1L));
+        try {
+            em.getTransaction().begin();
+            List<Student> allStudents = studentRepository.getAllStudents();
+            em.getTransaction().commit();
+            return mapToStudentDtos(allStudents);
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            return List.of(); // returns empty list
+        }
+    }
 
-        List<Student> allStudents = studentRepository.getAllStudents();
-        em.getTransaction().commit();
-        return mapToStudentDtos(allStudents);
+    public StudentDto getStudentWithId(Long id) {
+        try {
+            em.getTransaction().begin();
+            Student student = studentRepository.findStudentWithId(id).orElseThrow();// todo; custom exception
+            em.getTransaction().commit();
+            return new StudentDto(student.getAlbumId(), student.getFirstName(), student.getSecondName(), student.getLastName(), student.getEmail(),
+                    student.getUniversityEmail(), student.getPhoneNumber(), student.getAlbumId());
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            return null;
+        }
     }
 
 
