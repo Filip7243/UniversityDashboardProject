442021feafe19561b99611a4cc2a1af70b8e6470
Filip <filip7243@gmail.com>
2022-11-03 16:14:45 +0100

    added getters setters to Professor

--- src/main/java/com/fxproject/unidashboard/model/Professor.java
+++ src/main/java/com/fxproject/unidashboard/model/Professor.java
@@ -28,4 +28,143 @@ public class Professor {
     private LocalDateTime createdAt; // time when student acc was created
     private Boolean isEnabled; // if not enabled, can't login to system
 
+    public Professor() {
+    }
+
+    public Professor(Long id, String firstName, String secondName, String lastName, String email, String universityEmail, LocalDateTime dateOfBirth, String placeOfBirth, String phoneNumber, String pesel, LocalDateTime dateOfEmployment, LocalDateTime createdAt, Boolean isEnabled) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.email = email;
+        this.universityEmail = universityEmail;
+        this.dateOfBirth = dateOfBirth;
+        this.placeOfBirth = placeOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.pesel = pesel;
+        this.dateOfEmployment = dateOfEmployment;
+        this.createdAt = createdAt;
+        this.isEnabled = isEnabled;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUniversityEmail() {
+        return universityEmail;
+    }
+
+    public void setUniversityEmail(String universityEmail) {
+        this.universityEmail = universityEmail;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public LocalDateTime getDateOfEmployment() {
+        return dateOfEmployment;
+    }
+
+    public void setDateOfEmployment(LocalDateTime dateOfEmployment) {
+        this.dateOfEmployment = dateOfEmployment;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Boolean getEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        isEnabled = enabled;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", universityEmail='" + universityEmail + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", placeOfBirth='" + placeOfBirth + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", pesel='" + pesel + '\'' +
+                ", dateOfEmployment=" + dateOfEmployment +
+                '}';
+    }
 }
