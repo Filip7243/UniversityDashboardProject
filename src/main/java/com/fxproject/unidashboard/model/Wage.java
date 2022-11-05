18b08e190cca55d00808d75ae41976793c95b696
Filip7243 <filip7243@gmail.com>
2022-11-05 12:33:42 +0100

    added: Wage model

--- src/main/java/com/fxproject/unidashboard/model/Wage.java
+++ src/main/java/com/fxproject/unidashboard/model/Wage.java
@@ -1,2 +1,19 @@
-package com.fxproject.unidashboard.model;public class Wage {
+package com.fxproject.unidashboard.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Wage {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private Double salary;
+    @ManyToOne
+    private UniversityEmployee employee;
+    private Double hourlyRate;
+    private Integer hoursWorked;
+
 }
