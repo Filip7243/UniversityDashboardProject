e87d49dc70f64ad69d5b5112f3813406f9dc0d6c
Filip <filip7243@gmail.com>
2022-11-07 15:08:04 +0100

    added default implementation to DefaultRepository save() method

--- src/main/java/com/fxproject/unidashboard/repository/DefaultRepository.java
+++ src/main/java/com/fxproject/unidashboard/repository/DefaultRepository.java
@@ -4,15 +4,31 @@ import com.fxproject.unidashboard.model.UniversityDepartment;
 import com.fxproject.unidashboard.model.UniversityMember;
 import com.fxproject.unidashboard.utils.HibernateUtils;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface DefaultRepository<E, T> { // E - model T - id type
 
-    void save(E record);
+    EntityManager em = HibernateUtils.getEntityManager();
+
+    default void save(E record) {
+        var transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.persist(record);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+
+    }
+
     void removeWithId(T id);
+
     Optional<E> findWithId(T id);
+
     List<E> findAll();
 
 }
