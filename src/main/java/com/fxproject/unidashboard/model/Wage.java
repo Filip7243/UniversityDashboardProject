41aabbd113cb2687b67b12fbbf41d1bdefaf2be9
Filip <filip7243@gmail.com>
2022-11-07 14:05:30 +0100

    added getters, setters and constructors

--- src/main/java/com/fxproject/unidashboard/model/Wage.java
+++ src/main/java/com/fxproject/unidashboard/model/Wage.java
@@ -19,4 +19,63 @@ public class Wage {
     private Integer hoursWorked;
     private LocalDateTime payday;
 
+    public Wage(Long id, Double salary, UniversityMember employee, Double hourlyRate, Integer hoursWorked, LocalDateTime payday) {
+        this.id = id;
+        this.salary = salary;
+        this.employee = employee;
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+        this.payday = payday;
+    }
+
+    public Wage() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public UniversityMember getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(UniversityMember employee) {
+        this.employee = employee;
+    }
+
+    public Double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(Double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public Integer getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(Integer hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public LocalDateTime getPayday() {
+        return payday;
+    }
+
+    public void setPayday(LocalDateTime payday) {
+        this.payday = payday;
+    }
 }
