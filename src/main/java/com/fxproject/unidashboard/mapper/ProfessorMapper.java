0325a76caee6c6e380ca1f046bc72edd645f3b68
Filip <filip7243@gmail.com>
2022-11-09 10:51:28 +0100

    added ProfessorService

--- src/main/java/com/fxproject/unidashboard/mapper/ProfessorMapper.java
+++ src/main/java/com/fxproject/unidashboard/mapper/ProfessorMapper.java
@@ -1,2 +1,29 @@
-package com.fxproject.unidashboard.mapper;public class ProfessorMapper {
+package com.fxproject.unidashboard.mapper;
+
+import com.fxproject.unidashboard.dto.ProfessorDto;
+import com.fxproject.unidashboard.model.Professor;
+
+import java.util.ArrayList;
+
+public class ProfessorMapper {
+
+    private ProfessorMapper() {
+    }
+
+    public static Professor mapToProfessor(ProfessorDto professorDto) {
+        Professor professor = new Professor();
+        professor.setFirstName(professorDto.getFirstName());
+        professor.setSecondName(professorDto.getSecondName());
+        professor.setLastName(professorDto.getLastName());
+        professor.setEmail(professorDto.getEmail());
+        professor.setDateOfBirth(professorDto.getDateOfBirth());
+        professor.setPlaceOfBirth(professorDto.getPlaceOfBirth());
+        professor.setPhoneNumber(professorDto.getPhoneNumber());
+        professor.setPesel(professorDto.getPesel());
+        professor.setAcademicTitle(professorDto.getAcademicTitle());
+        professor.setYears(new ArrayList<>());
+        professor.setSubjects(new ArrayList<>());
+        return professor;
+    }
+
 }
--- src/main/java/com/fxproject/unidashboard/service/ProfessorService.java
+++ src/main/java/com/fxproject/unidashboard/service/ProfessorService.java
@@ -1,2 +1,46 @@
-package com.fxproject.unidashboard.service;public class ProfessorService {
+package com.fxproject.unidashboard.service;
+
+import com.fxproject.unidashboard.dto.ProfessorDto;
+import com.fxproject.unidashboard.email.EmailAddressGenerator;
+import com.fxproject.unidashboard.model.Professor;
+import com.fxproject.unidashboard.model.Role;
+import com.fxproject.unidashboard.model.UniversityAccount;
+import com.fxproject.unidashboard.repository.ProfessorRepository;
+import com.fxproject.unidashboard.repository.UniversityAccountRepository;
+import com.fxproject.unidashboard.repository.impl.ProfessorRepositoryImpl;
+import com.fxproject.unidashboard.repository.impl.UniversityAccountRepositoryImpl;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.UUID;
+
+import static com.fxproject.unidashboard.email.EmailAddressGenerator.generateUniversityEmailForProfessor;
+import static com.fxproject.unidashboard.mapper.ProfessorMapper.mapToProfessor;
+
+public class ProfessorService {
+
+    private final ProfessorRepository professorRepository = new ProfessorRepositoryImpl();
+    private final UniversityAccountRepository accountRepository = new UniversityAccountRepositoryImpl();
+
+    public void addProfessor(ProfessorDto professorDto) {
+        Professor professor = mapToProfessor(professorDto);
+        professorRepository.save(professor);
+        professor.setUniversityAccount(createUniversityAccountForProfessor(professor));
+        professorRepository.save(professor);
+    }
+
+    private UniversityAccount createUniversityAccountForProfessor(Professor professor) {
+        UniversityAccount account = new UniversityAccount();
+        account.setMember(professor);
+        account.setUniversityEmail(generateUniversityEmailForProfessor(professor));
+        account.setPassword(UUID.randomUUID().toString()); // this password will be snet on email then must be changed
+        account.setRole(Role.ROLE_PROFESSOR);
+        account.setCreatedAt(LocalDateTime.now());
+        account.setEnabled(false); // default false, after confirm email will be true
+        accountRepository.save(account);
+        return account;
+    }
+
+
 }
