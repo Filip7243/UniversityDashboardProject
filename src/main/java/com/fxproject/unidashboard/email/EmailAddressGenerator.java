04f7565e8a61da5a22ead3969204db8d5b708325
Filip <filip7243@gmail.com>
2022-11-09 10:50:52 +0100

    added EmailAddressGenerator

--- src/main/java/com/fxproject/unidashboard/email/EmailAddressGenerator.java
+++ src/main/java/com/fxproject/unidashboard/email/EmailAddressGenerator.java
@@ -1,2 +1,17 @@
-package com.fxproject.unidashboard.email;public class EmailAdressGenerator {
+package com.fxproject.unidashboard.email;
+
+import com.fxproject.unidashboard.model.Professor;
+
+import java.util.Random;
+
+public class EmailAddressGenerator {
+
+    public static String generateUniversityEmailForProfessor(Professor professor) {
+        char firstLetterName = professor.getFirstName().toLowerCase().charAt(0);
+        char lastLetterName = professor.getLastName().toLowerCase().charAt(0);
+        Random random = new Random();
+        int randomNumber = random.nextInt(10000) + 1;
+
+        return String.valueOf(firstLetterName) + String.valueOf(lastLetterName) + randomNumber + "@.prof.uni.pl";
+    }
 }
