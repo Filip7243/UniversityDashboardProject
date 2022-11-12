417f2a4adaccb1dc4b2815c56dad5d0352105aae
Filip7243 <filip7243@gmail.com>
2022-11-12 14:43:40 +0100

    refactored all methods

--- src/main/java/com/fxproject/unidashboard/repository/impl/FieldOfStudyRepositoryImpl.java
+++ src/main/java/com/fxproject/unidashboard/repository/impl/FieldOfStudyRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.fxproject.unidashboard.model.UniversityDepartment;
 import com.fxproject.unidashboard.repository.FieldOfStudyRepository;
 import com.fxproject.unidashboard.utils.HibernateUtils;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
 
 import java.util.List;
@@ -18,67 +19,118 @@ public class FieldOfStudyRepositoryImpl implements FieldOfStudyRepository {
     @Override
     public void removeWithId(Long id) {
         FieldOfStudy fieldOfStudy = findWithId(id).orElseThrow();//todo: custom exception
-        em.getTransaction().begin();
-        em.remove(fieldOfStudy);
-        em.getTransaction().commit();
+
+        var transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.remove(fieldOfStudy);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e.getMessage());
+        }
+
     }
 
     @Override
     public Optional<FieldOfStudy> findWithId(Long id) {
-        em.getTransaction().begin();
-        TypedQuery<FieldOfStudy> query =
-                em.createQuery("SELECT f FROM FieldOfStudy f WHERE f.id = :id", FieldOfStudy.class);
-        query.setParameter("id", id);
-        em.getTransaction().commit();
-        return Optional.of(query.getSingleResult());
+        var transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            TypedQuery<FieldOfStudy> query =
+                    em.createQuery("SELECT f FROM FieldOfStudy f WHERE f.id = :id", FieldOfStudy.class);
+            query.setParameter("id", id);
+            transaction.commit();
+            return Optional.of(query.getSingleResult());
+        } catch (Exception e) {
+            transaction.rollback();
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<FieldOfStudy> findAll() {
-        em.getTransaction().begin();
-        List<FieldOfStudy> allFieldsOfStudy =
-                em.createQuery("SELECT f FROM FieldOfStudy f", FieldOfStudy.class).getResultList();
-        em.getTransaction().commit();
-        return allFieldsOfStudy;
+        var transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            List<FieldOfStudy> allFieldsOfStudy =
+                    em.createQuery("SELECT f FROM FieldOfStudy f", FieldOfStudy.class).getResultList();
+            transaction.commit();
+            return allFieldsOfStudy;
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e.getMessage());
+            return List.of();
+        }
     }
 
     @Override
     public Optional<FieldOfStudy> findFieldOfStudyByName(String name) {
-        em.getTransaction().begin();
-        TypedQuery<FieldOfStudy> query =
-                em.createQuery("SELECT f FROM FieldOfStudy f WHERE f.name = :name", FieldOfStudy.class);
-        query.setParameter("name", name);
-        em.getTransaction().commit();
-        return Optional.of(query.getSingleResult());
+        var transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            TypedQuery<FieldOfStudy> query =
+                    em.createQuery("SELECT f FROM FieldOfStudy f WHERE f.name = :name", FieldOfStudy.class);
+            query.setParameter("name", name);
+            transaction.commit();
+            return Optional.of(query.getSingleResult());
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e.getMessage());
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<FieldOfStudy> findFieldOfStudyByType(TypeOfStudy type) {
-        em.getTransaction().begin();
-        TypedQuery<FieldOfStudy> query =
-                em.createQuery("SELECT f FROM FieldOfStudy f WHERE f.type = :type", FieldOfStudy.class);
-        query.setParameter("type", type);
-        em.getTransaction().commit();
-        return query.getResultList();
+        var transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            TypedQuery<FieldOfStudy> query =
+                    em.createQuery("SELECT f FROM FieldOfStudy f WHERE f.type = :type", FieldOfStudy.class);
+            query.setParameter("type", type);
+            transaction.commit();
+            return query.getResultList();
+        } catch (Exception e) {
+            transaction.rollback();
+            return List.of();
+        }
+
     }
 
     @Override
     public Optional<UniversityDepartment> findFieldOfStudyDepartment(Long id) {
-        em.getTransaction().begin();
-        TypedQuery<UniversityDepartment> query =
-                em.createQuery("SELECT f.department FROM FieldOfStudy f WHERE f.id = :id", UniversityDepartment.class);
-        query.setParameter("id", id);
-        em.getTransaction().commit();
-        return Optional.of(query.getSingleResult());
+        var transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            TypedQuery<UniversityDepartment> query =
+                    em.createQuery("SELECT f.department FROM FieldOfStudy f WHERE f.id = :id", UniversityDepartment.class);
+            query.setParameter("id", id);
+            transaction.commit();
+            return Optional.of(query.getSingleResult());
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e.getMessage());
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<FieldOfStudy> findFieldsOfStudyByDepartment(UniversityDepartment department) {
-        em.getTransaction().begin();
-        TypedQuery<FieldOfStudy> query =
-                em.createQuery("SELECT f FROM FieldOfStudy f WHERE f.department = :department", FieldOfStudy.class);
-        query.setParameter("department", department);
-        em.getTransaction().commit();
-        return query.getResultList();
+        var transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            TypedQuery<FieldOfStudy> query =
+                    em.createQuery("SELECT f FROM FieldOfStudy f WHERE f.department = :department", FieldOfStudy.class);
+            query.setParameter("department", department);
+            transaction.commit();
+            return query.getResultList();
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e.getMessage());
+            return List.of();
+        }
     }
+
 }
