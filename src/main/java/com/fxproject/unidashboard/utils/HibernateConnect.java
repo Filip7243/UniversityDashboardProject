7adb73e57663e59e22f56a3d94ce25502b292adf
Filip <filip7243@gmail.com>
2023-01-14 19:24:40 +0100

    changes for jar

--- src/main/java/com/fxproject/unidashboard/utils/HibernateConnect.java
+++ src/main/java/com/fxproject/unidashboard/utils/HibernateConnect.java
@@ -1,14 +1,25 @@
 package com.fxproject.unidashboard.utils;
 
+import com.fxproject.unidashboard.HelloApplication;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Objects;
 
 public class HibernateConnect {
 
-    private static final SessionFactory SESSION_FACTORY = new Configuration().configure().buildSessionFactory();
+    private static final SessionFactory SESSION_FACTORY;
 
-    private HibernateConnect(){}
+    static {
+        Configuration configuration = new Configuration();
+        SESSION_FACTORY = configuration.configure("hibernate.cfg.xml").buildSessionFactory();
+
+    }
+
+    private HibernateConnect() {
+    }
 
     public static Session openSession() {
         return SESSION_FACTORY.openSession();
