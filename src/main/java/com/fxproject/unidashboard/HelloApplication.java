9ad6727c503edb11b3b715de40e79cb7b365f8f8
Filip <filip7243@gmail.com>
2023-01-17 23:30:22 +0100

    last changes

--- src/main/java/com/fxproject/unidashboard/HelloApplication.java
+++ src/main/java/com/fxproject/unidashboard/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.fxproject.unidashboard;
 
 import com.fxproject.unidashboard.schema.InsertData;
+import com.fxproject.unidashboard.utils.HibernateConnect;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,7 +14,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        new InsertData();
+        HibernateConnect.openSession();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1150, 650);
         stage.initStyle(StageStyle.UNDECORATED);
